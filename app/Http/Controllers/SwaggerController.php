02acbe8201de30b259d2b2aca84842983d413d4d
susana <susanabeltran679@gmail.com>
2026-04-09 00:49:34 -0600

    fix: swagger error

--- app/Http/Controllers/SwaggerController.php
+++ app/Http/Controllers/SwaggerController.php
@@ -2,26 +2,5 @@
 
 namespace App\Http\Controllers;
 
-/**
- * @OA\Info(
- *     title="Marketplace de Productos para Mascotas API",
- *     version="1.0.0",
- *     description="API REST para marketplace de mascotas"
- * )
- *
- * @OA\Server(
- *     url="http://127.0.0.1:8000/api",
- *     description="Servidor local"
- * )
- *
- * @OA\SecurityScheme(
- *     securityScheme="sanctum",
- *     type="apiKey",
- *     in="header",
- *     name="Authorization",
- *     description="Enter token in format: Bearer {token}"
- * )
- */
-class SwaggerController extends Controller
-{
-}
+
+class SwaggerController extends Controller {}
